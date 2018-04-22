ec1c528e3494a2bd37f2680dde0f7682b61f6ce7
faizaan-e-nabuwat <38571727+faizaan-e-nabuwat@users.noreply.github.com>
2018-04-22 05:40:52 +0100

    Add files via upload

--- dataformat.m
+++ dataformat.m
@@ -7,33 +7,59 @@ row_1=val(1,:);
 row_2=val(2,:);
 t= 1:(length(row_1))
 t= t/30
+fs = 300;
+s = 1:(length(row_1))
+s = t/300 
+ecg1 = row_1;
+mean_ecg1 = mean(ecg1);
+ecg1 = ecg1 - mean_ecg1
+%{
+plot(ecg1);
+plot(row_1);
+ plot(t,ecg1,'g',t,row_1,'b')
+ title("ECG with and without mean")
+
+%}
+figure
+subplot(2,2,1)
+plot(row_1,'b')
+title("ECG signal with mean removed")
+subplot(2,2,2)
+plot(ecg1,'g')
+title("ECG signal with mean")
+subplot(2,2,[3 4])
+plot(t,row_1,'g',t,ecg1,'b')
+title("Both ECG signals for sake of comparity")
+% removing mean form the ecg signal
+% mean is taking from the signal and removed it is
+% more centred towards the zero axis
+
+%butter worth low and high pass filter to remove base line drift
+%and other unwanted frequencies
+
+
+
+
+
 
 
 
 %{
 figure
 plot(t,row_1)
+
 figure
 plot(t, peaks_row1)
 
-}%
 
 
-
-
-
-
-
-
-
-%{
 figure
 plot(A)
 A = subplot(2,2)
 subplot(1,1), plot(row1)
 subplot(1,2), plot(peaks_row1)
 
-%}
+
 %row1= transpose(row1);
 
 %neg_to_pos=zeros(0,sizerow1);
